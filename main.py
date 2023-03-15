5ae6eaa66162bb4145a964ba26dd4133d3c5d657
Jipr <64846573+Jipr@users.noreply.github.com>
2023-03-15 17:31:24 +0100

    t

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import numpy 
+import pandas as pd
+import openpyxl
+
+f = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]],
+                  index=['one', 'two', 'three'], columns=['a', 'b', 'c'])
