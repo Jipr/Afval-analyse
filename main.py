d6636ea9fa278918e3bbaa350b9b6f1b9b66bb9d
Jipr <64846573+Jipr@users.noreply.github.com>
2023-03-16 09:44:57 +0100

    Excel werkt

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import openpyxl
 import numpy
-f = pd.DataFrame([[15, 28, 40], [12, 22, 32], [31, 32, 33]],
-                  index=['one', 'two', 'three'], columns=['a', 'b', 'c'])
+a = 10
+b = 23
+c = 55
+
+
+f = pd.DataFrame([[a], [b], [c]],
+                  index=['Class 1', 'Class 2', 'Class 3'], columns=['Amount'])
 f.to_excel('OSPAR_Test.xlsx', sheet_name='First_try')
