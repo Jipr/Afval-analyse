ff98c3267a5939e280a43fc732ecf5fb361a94d5
Jipr <64846573+Jipr@users.noreply.github.com>
2023-03-16 09:26:52 +0100

    t

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import openpyxl
-
-f = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]],
+import numpy
+f = pd.DataFrame([[15, 28, 40], [12, 22, 32], [31, 32, 33]],
                   index=['one', 'two', 'three'], columns=['a', 'b', 'c'])
+f.to_excel('OSPAR_Test.xlsx', sheet_name='First_try')
