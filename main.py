caa4cc3e1eb32c6e535f33565ffc44359ff8397a
Jipr <64846573+Jipr@users.noreply.github.com>
2023-03-29 16:57:11 +0200

    Object detection code 5

--- main.py
+++ main.py
@@ -37,23 +37,6 @@ def commit_to_dataBase(object_class):
     f = pd.DataFrame([[a], [b], [c]], index=['Class 1', 'Class 2', 'Class 3'], columns=['Amount'])
     f.to_excel('OSPAR_Test.xlsx', sheet_name='First_try')
 
-def create_box_annotator():
-    box_annotator = sv.BoxAnnotator(
-    thickness=2,
-    text_thickness=1,
-    text_scale=0.5
-    )
-    return box_annotator
-
-def create_lineZone_annotator(line_zone):
-    line_annotator = sv.LineZoneAnnotator(
-        thickness=2,
-        text_thickness=1,
-        text_scale=0.5
-    )
-    return line_annotator 
-    
-
 def create_labels(model, detections):
     labels = [
         f"# {tracker_id}{model.model.names[class_id]} {confidence:0.2f}"
@@ -65,32 +48,29 @@ def create_labels(model, detections):
 
 #Main code#-------------------------------------------
 
-def new_func():
-    return True
-
 while(detect):
-    model = YOLO("yolov8l.pt") #Trained model / path to local file
     line_counter  = sv.LineZone(start=START, end=END)
+    line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
+    box_annotator = sv.BoxAnnotator(thickness=2,text_thickness=1,text_scale=0.5)
+    model = YOLO("yolov8l.pt") #Trained model / path to local file
 
-    line_annotator = create_lineZone_annotator(line_counter)
-    box_annotator = create_box_annotator()
-
-    for result in model.track(source=0, show=new_func(), stream=False): # Detect frame by frame
+    for result in model.track(source=0, show=video_show, stream=True, agnostic_nms=True ): # Detect frame by frame
         frame = result.orig_img 
         detections = sv.Detections.from_yolov8(result)
         if result.boxes.id is not None:
             detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
 
+        #detections = detections[(detections.class_id != 60) & (detections.class_id != 0)]
         labels = create_labels(model,detections)
         frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
-    
-        line_counter.trigger(detections=detections) 
-        line_annotator.annotate(frame=frame, line_counter=line_counter)
 
-        if video_show == True: # Display the resulting frame
-            cv2.imshow('Litter ditection', frame)  
-             
-        if cv2.waitKey(1) & 0xFF == ord('q'): # Quit script button
+        line_counter.trigger(detections=detections) 
+        frame = line_annotator.annotate(frame=frame, line_counter=line_counter)
+
+        #if video_show == True: # Display the resulting frame
+        #    cv2.imshow('Litter ditection', frame)  
+
+        if cv2.waitKey(30) == 27: # Quit script with esc
             release_cam()
             detect = False
             break
