8b222c08dda4776b44a69f9c58b33bb4a5c4a272
Jipr <64846573+Jipr@users.noreply.github.com>
2023-04-19 16:37:45 +0200

    Code pc Parshva
    Object detectie werkt

--- main2.py
+++ main2.py
@@ -11,6 +11,12 @@ import pandas as pd
 import xlrd
 from ultralytics import YOLO
 import supervision as sv
+from datetime import date
+
+#Variables to change----------------------------------------------
+video_file_name = "VID_20230406_143256~3.mp4"
+
+
 
 #Global variables#------------------------------------------------
 xlrd.xlsx.ensure_elementtree_imported(False, None)
@@ -25,6 +31,9 @@ sheet_number = int(value + 1)
 LINE_START = sv.Point(0, 675)
 LINE_END = sv.Point(1080, 657)
 
+today = date.today()
+
+
 #Objects ID's
 object1_id = 0 # Object ID of: Drankblikjes
 object1_name = 'Drankblikjes'
@@ -38,24 +47,38 @@ object5_id = 4 # Object ID of: Voedselverpakkingen
 object5_name = 'Voedselverpakkingen'
 
 
+global object1_in
+global object2_in
+global object3_in
+global object4_in
+global object5_in
+
+#Save video
+video = cv2.VideoCapture(video_file_name)
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+size = (frame_width, frame_height)
+# Below VideoWriter object will create a frame of above defined The output is stored in BatchNumber'x' file.
+video_result = cv2.VideoWriter(f'BatchNumber{sheet_number}.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
+
 #Functions#-------------------------------------------------------
 def commit_to_dataBase(object1_count,object2_count,object3_count,object4_count,object5_count):
     f = pd.DataFrame([[object1_count], [object2_count], [object3_count], [object4_count], [object5_count]], index=[object1_name, object2_name, object3_name, object4_name, object5_name], columns=['Amount'])
-    
-  
     with pd.ExcelWriter('OSPAR_Test.xlsx', engine="openpyxl", mode='a') as writer:  
         f.at[object1_name, 'Sheet number'] = sheet_number
+        f.at[object1_name, 'Video name'] = video_file_name
+        f.at[object1_name, 'Date'] = today
         f.to_excel(writer, sheet_name= f'Sheet{sheet_number}')
         
 
 def create_labels(model, detections):
     labels = [
-        f"# {class_id}{model.model.names[class_id]} {confidence:0.2f}"
-        for _, confidence, class_id, tracker_id
-        in detections 
+        f"# {tracker_id}{model.model.names[class_id]} {confidence:0.2f}"
+        for _, _, confidence, class_id, tracker_id
+        in detections
     ]
     return labels
-    
+
 
 #Class#------------------------------------------------------------
 class object_line_counter():
@@ -79,64 +102,70 @@ class object_line_counter():
 
 #Main function#----------------------------------------------------
 def main():
-    #Initialisation -----------------------------------------------
-    line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
-    line_counter1 = sv.LineZone(start=LINE_START, end=LINE_END)
-    line_counter2 = sv.LineZone(start=LINE_START, end=LINE_END)
-    line_counter3 = sv.LineZone(start=LINE_START, end=LINE_END)
-    line_counter4 = sv.LineZone(start=LINE_START, end=LINE_END)
-    line_counter5 = sv.LineZone(start=LINE_START, end=LINE_END)
-    
-    line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
-    box_annotator = sv.BoxAnnotator(thickness=2,text_thickness=1,text_scale=0.5)
+    try:
+        #Initialisation -----------------------------------------------
+        line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
+        line_counter1 = sv.LineZone(start=LINE_START, end=LINE_END)
+        line_counter2 = sv.LineZone(start=LINE_START, end=LINE_END)
+        line_counter3 = sv.LineZone(start=LINE_START, end=LINE_END)
+        line_counter4 = sv.LineZone(start=LINE_START, end=LINE_END)
+        line_counter5 = sv.LineZone(start=LINE_START, end=LINE_END)
 
-    # Create class objects ----------------------------------------
-    object1 = object_line_counter(object1_id, line_counter1)
-    object2 = object_line_counter(object2_id, line_counter2)
-    object3 = object_line_counter(object3_id, line_counter3)
-    object4 = object_line_counter(object4_id, line_counter4)
-    object5 = object_line_counter(object5_id, line_counter5)
+        line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
+        box_annotator = sv.BoxAnnotator(thickness=2,text_thickness=1,text_scale=0.5)
 
-    model = YOLO("model_2023-04-13_1547/best.pt") #Load YOLOv8 model 
+        # Create class objects ----------------------------------------
+        object1 = object_line_counter(object1_id, line_counter1)
+        object2 = object_line_counter(object2_id, line_counter2)
+        object3 = object_line_counter(object3_id, line_counter3)
+        object4 = object_line_counter(object4_id, line_counter4)
+        object5 = object_line_counter(object5_id, line_counter5)
 
-
-
-    for result in model.track(source="C:/Users/Jip Rasenberg/Noria/Noria Internship - Jip Rasenberg/2. Working Folder/7. Data/2. Video data/4-3/Afgesneden/VID_20230406_131902~3.mp4", show=True, stream=True, agnostic_nms=True):
-        
-        frame = result.orig_img
-
-        #Detect al objects-----------------------------------------
-        detections = sv.Detections.from_yolov8(result) #Get object detections from result
-        if result.boxes.id is not None: #If nothing is detected in frame
-            detections.tracker_id = result.boxes.id.cpu().numpy().astype(int) 
-        labels = create_labels(model,detections) #Create labels  
-
-        #Detect specific objects-----------------------------------
-        object1_in,object1_out = object1.detections(result)
-        object2_in,object2_out = object2.detections(result)
-        object3_in,object3_out = object3.detections(result)
-        object4_in,object4_out = object4.detections(result)
-        object5_in,object5_out = object5.detections(result)
-
-        #Show boxes and line counter--------------------------------
-        frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
-        
-        line_counter.trigger(detections=detections)
-        line_annotator.annotate(frame=frame, line_counter=line_counter)
+        model = YOLO("model_2023-04-13_1547/best.pt") #Load YOLOv8 model
 
 
-        #print('object1_in',object1_in, 'object1_out',object1_out)
-        #print('object2_in',object2_in, 'object2_out',object2_out)
-        #print('object3_in',object3_in, 'object3_out',object3_out)
-        #print('object4_in',object4_in, 'object4_out',object4_out)
-        #print('object5_in',object5_in, 'object5_out',object5_out)
 
-        cv2.imshow("yolov8", frame)
+        for result in model.track(source=video_file_name, show=True, stream=True, agnostic_nms=True):
 
-        if (cv2.waitKey(30) == 27): #If ESC is pressed, the counters get uploaded to excel and script shuts down
-            commit_to_dataBase(object1_in,object2_in,object3_in,object4_in,object5_in)
-            break
+            frame = result.orig_img
 
+            #Detect al objects-----------------------------------------
+            detections = sv.Detections.from_yolov8(result) #Get object detections from result
+            if result.boxes.id is not None: #If nothing is detected in frame
+                detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
+            labels = create_labels(model,detections) #Create labels
+
+            #Detect specific objects-----------------------------------
+            object1_in,object1_out = object1.detections(result)
+            object2_in,object2_out = object2.detections(result)
+            object3_in,object3_out = object3.detections(result)
+            object4_in,object4_out = object4.detections(result)
+            object5_in,object5_out = object5.detections(result)
+
+            #Show boxes and line counter--------------------------------
+            frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
+
+            line_counter.trigger(detections=detections)
+            line_annotator.annotate(frame=frame, line_counter=line_counter)
+
+
+            #print('object1_in',object1_in, 'object1_out',object1_out)
+            #print('object2_in',object2_in, 'object2_out',object2_out)
+            #print('object3_in',object3_in, 'object3_out',object3_out)
+            #print('object4_in',object4_in, 'object4_out',object4_out)
+            #print('object5_in',object5_in, 'object5_out',object5_out)
+            video_result.write(frame) #Write the frame into the file BatchNumber'x' file.
+            small = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+            cv2.imshow("yolov8", small)
+
+            if (cv2.waitKey(30) == 27):
+                cv2.destroyAllWindows()
+                break
+    finally: #If video is finished, or ESC is pressed, the counters are uploaded to the excel file with sheet name Sheet{sheet_number}
+        commit_to_dataBase(object1_in, object2_in, object3_in, object4_in, object5_in)
+        video_result.release()
+        print("The video, and counters are successfully saved")
 
 if __name__ == "__main__":
-    main() 
+    main()
+
